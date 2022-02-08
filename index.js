d8a539f4a33c08971f88a3d8e4802b85363b1aae
bsamu <bsamuel0220@gmail.com>
2022-02-08 10:26:46 +0100

    ver1

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// insomnia (google) - insomnia.rest - requestek küldése
+// terminalból request curl (google)
+// thunder egy extension requestekre
+
+const cars = [
+    {
+        id: 1,
+        model: "BMW",
+        maxSpeed: 150
+    },
+    {
+        id: 2,
+        model: "Toyota",
+        maxSpeed: 120
+    },
+    {
+        id: 3,
+        model: "Tesla",
+        maxSpeed: 170
+    }
+]
+
+app.get('/api/cars', (req, res) => {
+    res.json(cars) // ált nem app.send-et használunk, az generikus
+})
+
+app.post('/api/cars', (req, res) => {
+    // http://localhost:3000/api/cars?name=Opel&speed=170 insomniaban post req
+    // ezt később átírjuk, url http://localhost:3000/api/cars body JSON, oda a query-k objectként
+    console.log(req.query); // _ 1. paraméterként kap vmit, de nem használjuk semmire
+    if (!req.query.name || !req.query.speed) return res.send("Missing parameters") // ha így csinálom, kell return res.send elé, így a szebb, de lehetne if-else, akkor nem kell return
+    const newCar = {
+        id: cars.length + 1,
+        model: req.query.name,
+        maxSpeed: req.query.speed
+    }
+    cars.push(newCar)
+    // vagy úgy push, hogy cars.push({ide a key-valuek})
+    res.send("DONE")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
