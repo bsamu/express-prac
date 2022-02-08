10efdc25a3f4c935465b8a1d39daf92c492332d8
bsamu <bsamuel0220@gmail.com>
2022-02-08 10:47:50 +0100

    workin'

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.use(express.json)
-// ez mindig kell, ha valahonnan egy json típusú dolog érkezik, alapból felparseolja, belerakja req.bodyba
-
-// insomnia (google) - insomnia.rest - requestek küldése
-// terminalból request curl (google)
-// thunder egy extension requestekre
+app.use(express.json())
 
 const cars = [
     {
@@ -28,34 +23,19 @@ const cars = [
 
 app.get('/api/cars', (req, res) => {
     res.json(cars)
-    // ált nem app.send-et használunk, az generikus
 })
 
 app.post('/api/cars', (req, res) => {
-    // http://localhost:3000/api/cars?name=Opel&speed=170 insomniaban post req
-    // ezt később átírjuk, url http://localhost:3000/api/cars body JSON, oda a query-k objectként
-    // console.log(req.query); // _ 1. paraméterként kap vmit, de nem használjuk semmire
     console.log(req.body);
-    // if (!req.query.name || !req.query.speed) return res.send("Missing parameters") // ha így csinálom, kell return res.send elé, így a szebb, de lehetne if-else, akkor nem kell return
-    // const newCar = {
-    //     id: cars.length + 1,
-    //     model: req.query.name,
-    //     maxSpeed: req.query.speed
-    // }
-    if (!req.body.name || !req.body.speed) return res.send("Missing parameters") // ha így csinálom, kell return res.send elé, így a szebb, de lehetne if-else, akkor nem kell return
-    if (!req.body.name || !req.body.speed) return res.send("Missing parameters") // ha így csinálom, kell return res.send elé, így a szebb, de lehetne if-else, akkor nem kell return
+
+    if (!req.body.name || !req.body.speed) return res.send("Missing parameters")
+
     const newCar = {
         id: cars.length + 1,
         model: req.body.name,
         maxSpeed: req.body.speed
     }
-    // const newCar = {
-    //     id: cars.length + 1,
-    //     model: req.query.name,
-    //     maxSpeed: req.query.speed
-    // }
     cars.push(newCar)
-    // vagy úgy push, hogy cars.push({ide a key-valuek})
     res.send("DONE")
 })
 
